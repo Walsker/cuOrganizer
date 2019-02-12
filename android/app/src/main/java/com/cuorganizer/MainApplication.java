a1bea9ab43fb216d29104ce91466c72743b97acd
Wal <wal.gatlei@gmail.com>
2019-02-12 17:53:46 -0500

    Now successfully builds on iOS

--- android/app/src/main/java/com/cuorganizer/MainApplication.java
+++ android/app/src/main/java/com/cuorganizer/MainApplication.java
@@ -3,6 +3,7 @@ package com.cuorganizer;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
           new VectorIconsPackage(),
           new RNFirebasePackage(),
           new RNFirebaseFirestorePackage(),
---
+++ ios/Pods/Target Support Files/Pods-cuOrganizerTests/Pods-cuOrganizerTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cuOrganizerTests : NSObject
+@end
+@implementation PodsDummy_Pods_cuOrganizerTests
+@end
